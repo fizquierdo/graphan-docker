e32f04087a16281f42d28fb9684423942d8f1bf0
Fernando Izquierdo <fer.izquierdo@gmail.com>
2017-05-14 20:09:02 +0100

    added radical preprocessing

---
+++ app/scripts/preprocessing/extract_radicals.rb
@@ -0,0 +1,42 @@
+require "csv"
+require 'nokogiri'
+require 'open-uri'
+
+def decompose(character, list_id = 0)
+	url = 'http://www.hanzicraft.com/character/' + character.to_s
+	url = URI.encode url
+	html = open(url)
+	page = Nokogiri::HTML(html.read)
+	decompositions = []
+	page.css("div.decompbox").each_with_index do |box, i|
+		# 0 decomposes in pairs
+		# 1 decomposes in radicals
+		# 2 decomposes following some graphical pattern
+		begin
+			han = nil
+			han, decomp = box.text.gsub("\n","").to_s.split('=>')
+			decompositions[i] = decomp.strip.split(',').map{|n|n.strip}
+		rescue
+			decompositions[i] = []
+		end
+	end
+	decompositions[list_id]
+end
+
+# Input tsv file with words in a column with header "simp"
+filename = ARGV[0]
+
+characters = []
+words =  CSV.read(filename, headers: true, skip_blanks: true, col_sep: "\t")
+words.each do |word|
+	simp = word["simp"]
+	simp.split('').each do |ch|
+		characters << ch unless characters.include? ch
+	end
+end
+
+puts "character,radicals"
+characters.each do |ch|
+	radicals = decompose(ch, 1).map{|rad| rad.split('(').first}.join(";")
+	puts ch + ',' + radicals
+end
--- app/scripts/preprocessing/preprocess_hsk.sh
+++ app/scripts/preprocessing/preprocess_hsk.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+
 # Merge all HSK files into a single
 HSKFILE=data/hsk_words.tsv
+HSK_RADICALS_FILE=data/hsk_radicals.csv
 rm -f $HSKFILE
+rm -f $HSK_RADICALS_FILE
 
+echo "Merging words"
 for LEVEL in 1 2 3 4 5 6
 do
 	ruby scripts/preprocessing/read_hsk_sources.rb ${LEVEL} >> $HSKFILE
 done
 
-# TODO Now that all have been merged we can invoke the decomposition
-#ruby scripts/scrapping/decomposer.rb ${LEVEL} > data/hsk${LEVEL}_radicals.csv
+echo "Extracting radicals from words"
+ruby scripts/preprocessing/extract_radicals.rb ${HSKFILE} > ${HSK_RADICALS_FILE}
