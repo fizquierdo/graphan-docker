76b8351fed344aa0c13b2c763763c9ae29bafbe7
Fernando Izquierdo <fer.izquierdo@gmail.com>
2017-05-21 13:25:42 +0100

    added sign-in tests

--- app/spec/app_spec.rb
+++ app/spec/app_spec.rb
@@ -11,9 +11,41 @@ describe "Sinatra app" do
 		Sinatra::Application
 	end
 
-	it 'Index page loads' do
-		get '/'
-		expect(last_response.body).to include('Home')
+	# Index
+	describe "Home page" do
+		it 'Home page loads' do
+			get '/'
+			expect(last_response.body).to include('Home')
+		end
+		it 'welcomes user if signed in' do
+			get '/', :username => 'Bob'
+			expect(last_response.body).to include('Hello Bob')
+		end
+		it 'Tells user to sign in if not logged-in' do
+			get '/', :username => 'unlogged-user'
+			expect(last_response.body).to include('Sign in if you have an account')
+		end
+	end
+
+	# Sign up
+	describe "Sign up page" do
+		it 'Signup page loads' do
+			get '/signup'
+			expect(last_response.body).to include('Sign Up for a new account')
+		end
+	end
+
+	# Sign in
+	it 'Signin page loads' do
+		get '/signin'
+		expect(last_response.body).to include('Sign in')
+		expect(last_response.body).to include('Username')
+	end
+
+	it 'does not signin a wrong user' do
+		post '/signin', {username: "idontexist", password: "not-a-pass"}
+		expect(last_response.body).to include('Sign in failed')
+		expect(last_request.path).to eq('/signin')
 	end
 
 end
