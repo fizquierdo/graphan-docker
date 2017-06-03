aa1e917d37899cacb4898fa369a54bd894ae46b1
Fernando Izquierdo <fer.izquierdo@gmail.com>
2017-06-03 12:04:23 +0100

    added scripts for import/export user progress

---
+++ app/scripts/prod/export_db_prod.rb
@@ -0,0 +1,16 @@
+require_relative '../../neo4j_api'
+
+cfg_file = File.join(File.dirname(__FILE__), '../../config/prod/config.yml')
+config = YAML.load_file(cfg_file)
+neo = Neo4j.new(config)
+
+export_dir = File.join(File.dirname(__FILE__), '../../data/exports')
+puts "Exporting DB user data to folder #{export_dir}"
+
+fname = "user_db.csv"
+fname = File.join(export_dir, 'user_db.csv')
+neo.export_users(fname)
+
+fname = "user_db_rels.csv"
+fname = File.join(export_dir, 'user_db_rels.csv')
+neo.export_users_rels(fname)
---
+++ app/scripts/prod/populate_db_prod.rb
@@ -0,0 +1,45 @@
+require_relative '../../neo4j_api'
+
+cfg_file = File.join(File.dirname(__FILE__), '../../config/prod/config.yml')
+config = YAML.load_file(cfg_file)
+neo = Neo4j.new(config)
+
+# Import data into DEV
+data_url = "https://raw.githubusercontent.com/fizquierdo/graphan-docker/master/app/data/"
+
+puts "Deleting previous DB"
+neo.clean
+puts "Creating Pinyin Blocks"
+neo.add_pinyin_blocks("data/pinyinchart.csv")
+puts "Adding radical list"
+neo.add_radicals("data/radical_list.csv")
+
+# Words
+puts "Importing words"
+neo.import_words(data_url+"hsk_words.tsv")
+
+# Pinyin
+puts "Linking pinyin blocks"
+neo.link_words_with_pinyin_blocks
+puts "Creating pinyin nodes"
+neo.create_pinyin_from_words
+puts "Creating tone combos from words"
+neo.create_tone_combos_from_words
+
+# Characters
+puts "Create character nodes from words"
+neo.create_characters_from_words
+puts "Add frequency to characters"
+neo.add_freq_rank_to_characters(data_url+"character_frequency.tsv")
+puts "Add character decomposition to character"
+neo.link_characters_to_radicals(data_url+"hsk_radicals.csv")
+
+# Backbone
+puts "Add backbone nodes"
+neo.add_backbone(data_url+"backbone.csv")
+
+### Import existing user data
+puts "Importing user data"
+neo.import_users("data/exports/user_db.csv")
+puts "Importing user data rels"
+neo.import_users_rels("data/exports/user_db_rels.csv")
